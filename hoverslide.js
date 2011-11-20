16cd0cf5fb72611d6f6eafe7a1de32b07ff992d0
Frank Lam <frlam9@gmail.com>
2011-11-20 00:15:01 -0800

    fixed for multiple instances

--- hoverslide.js
+++ hoverslide.js
@@ -3,43 +3,46 @@
 		if (isNaN(widthOffset)) {
 			widthOffset = 400;
 		}
-		var $d = $('<div style="width:100%;position:relative;">');
-		this.after($d);
+		this.each(function() {
+			var $d = $('<div style="width:100%;position:relative;">');
+			var $t = $(this);
+			$t.after($d);
 
-		this.clone(false).css({
-			width : "100%",
-			overflow : "hidden",
-			visibility : "hidden"
-		}).appendTo($d);
+			$t.clone(false).css({
+				width : "100%",
+				overflow : "hidden",
+				visibility : "hidden"
+			}).appendTo($d);
 
-		this.appendTo($d);
+			$t.appendTo($d);
 
-		this.css({
-			position : "absolute",
-			overflow : "hidden",
-			width : "100%",
-			top : -this.css("padding-top").replace("px","") - this.css("margin-top").replace("px",""),
-			left : -this.css("padding-left").replace("px","") - this.css("margin-left").replace("px","")
+			$t.css({
+				position : "absolute",
+				overflow : "hidden",
+				width : "100%",
+				top : -$t.css("padding-top").replace("px","") - $t.css("margin-top").replace("px",""),
+				left : -$t.css("padding-left").replace("px","") - $t.css("margin-left").replace("px","")
+			});
+			var anim = null;
+			$t.hover(
+				function() {
+					var $t = $(this), $p = $t.parent();
+					$t.width($p.width());
+					$t.height($p.height());
+					if (anim) {
+						anim.stop();
+					}
+					anim = $t.animate({width: $p.width()+widthOffset},400);
+				},
+				function() {
+					var $t = $(this), $p = $t.parent();
+					if (anim) {
+						anim.stop();
+					}
+					anim = $t.animate({width: $p.width()},400);
+				}
+			);
 		});
-
-		var anim = null;
-		return this.hover(
-			function() {
-				var $t = $(this), $p = $t.parent();
-				$t.width($p.width());
-				$t.height($p.height());
-				if (anim) {
-					anim.stop();
-				}
-				anim = $t.animate({width: $p.width()+widthOffset},400);
-			},
-			function() {
-				var $t = $(this), $p = $t.parent();
-				if (anim) {
-					anim.stop();
-				}
-				anim = $t.animate({width: $p.width()},400);
-			}
-		);
+		return this;
 	}
 }(jQuery));
